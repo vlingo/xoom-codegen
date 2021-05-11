0103c2664201b58f08f5100893a8a3efeaf53f2d
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-05-11 11:59:22 -0300

    Project structure and classes

---
+++ src/main/java/io/vlingo/xoom/codegen/template/ParameterKey.java
@@ -0,0 +1,29 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.codegen.template;
+
+public interface ParameterKey {
+
+  String value();
+
+  enum Defaults implements ParameterKey {
+    IMPORTS("IMPORTS"),
+    PACKAGE_NAME("PACKAGE_NAME");
+
+    private final String value;
+
+    Defaults(String value) {
+      this.value = value;
+    }
+
+    @Override
+    public String value() {
+      return null;
+    }
+  }
+}
---
+++ src/main/java/io/vlingo/xoom/codegen/template/TemplateStandard.java
@@ -0,0 +1,19 @@
+package io.vlingo.xoom.codegen.template;
+
+public interface TemplateStandard {
+
+  String resolveClassname();
+
+  String resolveClassname(final String name);
+
+  String resolveClassname(final TemplateParameters parameters);
+
+  String resolveClassname(final String name, final TemplateParameters parameters);
+
+  String resolveFilename(final TemplateParameters parameters);
+
+  String resolveFilename(final String name, final TemplateParameters parameters);
+
+  String retrieveTemplateFilename(final TemplateParameters parameters);
+
+}
