2d215e931552919d882558705d067f37d05a13e4
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-07-28 16:26:39 -0300

    Docker dialect

--- src/main/java/io/vlingo/xoom/codegen/dialect/Dialect.java
+++ src/main/java/io/vlingo/xoom/codegen/dialect/Dialect.java
@@ -16,6 +16,7 @@ import java.util.stream.Stream;
 public enum Dialect {
   C_SHARP("cs", new String[]{}, new String[]{}),
   F_SHARP("fs", new String[]{}, new String[]{}),
+  DOCKER("", new String[]{}, new String[]{}),
   REACTJS("js", new String[]{"src", "main", "frontend"}, new String[]{}),
   JAVA("java", new String[]{"src", "main", "java"}, new String[]{"src", "test", "java"}),
   KOTLIN("kt", new String[]{"src", "main", "kotlin"}, new String[]{"src", "test", "kotlin"});
